87b76af8e020550b7e25bfccf3229160beab40ac
Eri Higuchi <209428143+ehcy223@users.noreply.github.com>
2025-05-18 14:04:50 +0000

    １１章まで

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,16 @@
 class PostImage < ApplicationRecord
+  # ActiveStorageで画像アップロード機能
+  has_one_attached :image
+
+  # 投稿はユーザーと紐づいている（1:N の N 側）
   belongs_to :user
+
+  # 画像が存在しない場合にデフォルト画像を返すメソッド
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg' # app/assets/images/no_image.jpg を asset pipeline 経由で表示
+    end
+  end
 end
